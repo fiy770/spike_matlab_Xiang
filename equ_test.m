4badaf2f5dff4f8685813aa055448220b4f413e8
fiy770 <fiy770@yahoo.com.tw>
2017-03-15 00:44:29 +0800

    normalize

--- equ_test.m
+++ equ_test.m
@@ -1,17 +1,20 @@
 clear all
 close all
 a= linspace(0,20e-9,1000);  %Imem Imem_max=1.9774e-7
-b=5e-9;   %Ith
-c=5e-9;    %Iin
+b=0.5e-9;   %Ith
+c=6.5e-9;    %Iin
 d=2e-9;     %Itau
-e=0.1;     %slope of equation
+e=0.5;     %slope of equation
 I0= 2.178e-12; 
 K = 0.6777;            %0.7054;            % Subthreshold slope factor
 Ut= 25.9e-3;
 Ka		=(K*K)/(K+1);
+const=(0.1)*ones(1,1000)
+
 for i=1:length(a)   
     equ(i)=5e-7*((b*c-d*b-d*a(i)+e*a(i)*a(i))*(K/(5e-13*0.0259*(1+(b/a(i))))));%b*c+(e*(a(i)^2))-a(i)*d;
     normalize_equ(i)=5e-7*((b*c-d*b-d*a(i)+e*a(i)*a(i))*(K/(5e-13*0.0259*(1+(b/a(i))))))*(1/a(i));
+    diffequ(i)=e-((b*c)/((a(i)+b)^2));
 end
 lowest_value= b*c-e*(d/(2*e))^2;
 delta=(0.6777/(5e-13*0.0259))*5e-7;
@@ -22,11 +25,16 @@ Vtau=(Ut/K)*log((d)/I0);
 Vmem_c=log(center/I0)*(1/Ka)*Ut;
 figure(1);
 plot(a,equ);
- title('dImem VS Imem')
+ title('dImem*dt VS Imem')
  xlabel('Imem')	
  ylabel('dImem')	
 figure(2);
 plot(a,normalize_equ);
-title('dImem VS Imem')
-ylabel('dImem/Imem')	
+title('dImem*dt/Imem VS Imem')
+ylabel('dImem*dt/Imem')	
+xlabel('Imem')	
+figure(3)
+plot(a,diffequ,a,const)
+title('d(dImem*dt/Imem*delta)/dImem VS Imem')
+ylabel('d(dImem*dt/Imem*delta)/dImem')	
 xlabel('Imem')	
--- spike1.m
+++ spike1.m
@@ -20,7 +20,7 @@ tmax = 1;               % simulation time
 t    = 0:dt:tmax;       % time step vector
 % Circuit voltages
 Vlk		=0.2607%0.2607%0.2073;
-Vthr	=0.2762%0.3027%0.293;
+Vthr	=0.2078%0.3027%0.293;
 Cp      = 5e-13;  %-----         % Capacitor of adaptation circuit'
 Cmem    = 1e-12;  %-----         % Membrane capacitance of the neuron cell
 
@@ -64,7 +64,7 @@ Tau		=(Cmem*Ut)/(K*Itau);
 % Tau2	=(Cp*Ut)/(K*Itau_ahp);
 % Frequ(a,b)=0;
 % for in=1:length(Iin2)
-    Iin=4.45e-9%Iin2(in)
+    Iin=6.5e-9%Iin2(in)
 %for k=1:length(Iahp)
     Iahp=0;
 for j=2:length(t)
@@ -74,7 +74,7 @@ for j=2:length(t)
 %             Iahp(j)	=0;
 	end
 	if Vmem(j-1) < Vp
-		Ia(j-1)=0.1*Imem(j-1);%I0*exp((Ka*Vmem(j-1))/Ut);	%positive feedback current
+		Ia(j-1)=0.5*Imem(j-1);%I0*exp((Ka*Vmem(j-1))/Ut);	%positive feedback current
 % 		Iahp(j)=Iahp(j-1)-(dt/Tau2)*(Iahp(j-1));
         DImem(j)=(((Ith/Itau)*(Iin-Iahp-Itau)+((Ia(j-1)/Itau)-1-(Iahp/Itau))*Imem(j-1)+(Ia(j-1)/Itau)*Ith)*(dt/(Tau*(1+(Ith/Imem(j-1))))));
         Imem(j)=Imem(j-1)+DImem(j);%(((Ith/Itau)*(Iin-Iahp-Itau)+((Ia(j-1)/Itau)-1-(Iahp/Itau))*Imem(j-1)+(Ia(j-1)/Itau)*Ith)*(dt/(Tau*(1+(Ith/Imem(j-1))))));
@@ -87,7 +87,7 @@ for j=2:length(t)
         Vmem(j-1)   = Vp;
         Vmem(j)		= Vr;    
 		Imem(j)		= I0mem;
-		Ia(j-1) 	= 0.1*Imem(j);%I0*exp((Ka*Vmem(j-1))/Ut);
+		Ia(j-1) 	= 0.5*Imem(j);%I0*exp((Ka*Vmem(j-1))/Ut);
 %  		Iahp(j)=Iahp(j-1)+(dt/Tau2)*((Ith_ahp/Itau_ahp)*Ica-Iahp(j-1));
 %  		Vahp(j)=(Ut/K)*log((Iahp(j))/I0);
 		Tspike 		=j-1- Tspike_ref;
